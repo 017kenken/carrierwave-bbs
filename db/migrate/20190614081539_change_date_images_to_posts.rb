222a8caa4c9f4170305adff9326a141cb4f61b9e
生貝健人 <rayna@pckntrayna.local>
2019-06-15 00:11:19 +0900

    カラムのデータ型変更

---
+++ db/migrate/20190614081539_change_date_images_to_posts.rb
@@ -0,0 +1,5 @@
+class ChangeDateImagesToPosts < ActiveRecord::Migration[5.2]
+  def change
+    change_column :posts, :images, :json
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_14_035628) do
+ActiveRecord::Schema.define(version: 2019_06_14_081539) do
 
   create_table "posts", force: :cascade do |t|
-    t.string "images"
+    t.json "images"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
