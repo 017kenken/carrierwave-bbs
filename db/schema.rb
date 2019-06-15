c1d8ca9ceb0bf0b0a460e1313320cf13a5a99637
生貝健人 <rayna@pckntrayna.local>
2019-06-15 13:05:59 +0900

    placeモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_14_152806) do
+ActiveRecord::Schema.define(version: 2019_06_15_040440) do
 
   create_table "comments", force: :cascade do |t|
     t.string "auther"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2019_06_14_152806) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
+  create_table "places", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
